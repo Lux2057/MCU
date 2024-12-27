c9bb421ae0f57581c04119054ae0cee1b9cfd1f6
Lux2057 <sv9b7jilxypduzcrjnjn9wh77um9de@bots.bitbucket.org>
2024-12-27 22:15:45 +0300

    Greenhouse: test pump

--- Nano/Greenhouse/src/main.cpp
+++ Nano/Greenhouse/src/main.cpp
@@ -5,39 +5,54 @@
 #include "Sensors/LightSensor.h"
 
 #define PUMP_MOSFET_PIN 2
-#define LED_MOSFET_PIN 3
+#define LIGHT_MOSFET_PIN 3
 
 LED led = LED();
 OLED oled = OLED();
 TempAndHumidSensor tempAndHumid = TempAndHumidSensor(10, 50);
 LightSensor lightSensor = LightSensor(10, 50);
 
+bool PumpIsActive = false;
+bool LightIsActive = false;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PUMP_MOSFET_PIN, OUTPUT);
-  pinMode(LED_MOSFET_PIN, OUTPUT);
+  pinMode(LIGHT_MOSFET_PIN, OUTPUT);
 
   digitalWrite(PUMP_MOSFET_PIN, LOW);
-  digitalWrite(LED_MOSFET_PIN, LOW);
+  digitalWrite(LIGHT_MOSFET_PIN, LOW);
   digitalWrite(LED_BUILTIN, LOW);
 
   oled.Init();
   tempAndHumid.Init();
-  lightSensor.Init();  
+  lightSensor.Init();
 }
 
 void loop()
 {
   TempAndHumidSensor::Data tempAndHumid_data = tempAndHumid.Get_mean_data();
-
-  led.blink(LED_BUILTIN, 500, 1);
-
   float light_data = lightSensor.Get_mean_data();
 
-  led.blink(LED_BUILTIN, 500, 2);
+  if (PumpIsActive)
+  {
+    if (light_data < 120)
+    {
+      digitalWrite(PUMP_MOSFET_PIN, LOW);
+      PumpIsActive = false;
+    }
+  }
+  else
+  {
+    if (light_data > 100)
+    {
+      digitalWrite(PUMP_MOSFET_PIN, HIGH);
+      PumpIsActive = true;
+    }
+  }
 
-  oled.Clear();  
+  oled.Clear();
 
   String text[3];
   text[0] = "Light: " + String(light_data);

dd5f6c58556f4168f5e0ab311bcfc6cba63c1fbf
Lux2057 <sv9b7jilxypduzcrjnjn9wh77um9de@bots.bitbucket.org>
2025-03-26 19:50:21 +0300

    Greenhouse: I2C circuit

--- Arduino/WeAct_3_STM32F401CC/Greenhouse/src/main.cpp
+++ Arduino/WeAct_3_STM32F401CC/Greenhouse/src/main.cpp
@@ -10,35 +10,37 @@ LED built_in_led = LED(LED_BUILTIN);
 OLED oled = OLED();
 TempAndHumidSensor temp_and_humid_sensor = TempAndHumidSensor();
 LightSensor light_sensor = LightSensor();
-
-ServoControl servo = ServoControl(PC14);
+// ServoControl servo = ServoControl(PC14);
 
 void setup()
 {
-  servo.Init();
   built_in_led.Init();
   oled.Init();
+  // servo.Init();
   temp_and_humid_sensor.Init();
   light_sensor.Init();
 }
 
+int loop_index = 1;
+
 void loop()
 {
-  servo.SetAngle(180);
-  delay(500);
-  servo.SetAngle(0);
-/*   TempAndHumidSensor::Data tempAndHumid_data = temp_and_humid_sensor.Get_mean_data();
+  TempAndHumidSensor::Data tempAndHumid_data = temp_and_humid_sensor.Get_mean_data();
   float light_data = light_sensor.Get_mean_data();
 
   oled.Clear();
 
-  String text[3];
-  text[0] = "Light: " + String(light_data);
-  text[1] = "Temp: " + String(tempAndHumid_data.temperature);
-  text[2] = "Humid: " + String(tempAndHumid_data.humidity);
-  oled.Print(text, 3);
+  String text[5];
+  text[0] = "Loop number: " + String(loop_index);
+  text[1] = "";
+  text[2] = "Light: " + String(light_data);
+  text[3] = "Temp: " + String(tempAndHumid_data.temperature);
+  text[4] = "Humid: " + String(tempAndHumid_data.humidity);
+  oled.Print(text, 5);
 
-  built_in_led.blink(50, 50, 3); */
+  built_in_led.blink(50, 50, 3);
 
-  delay(1000);
+  loop_index++;
+
+  delay(200);
 }
